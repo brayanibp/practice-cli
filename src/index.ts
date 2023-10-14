3f0302bfa0d013b69f808c725ed5a44a99853380
Brayan Bernal <brayanbernal0710@gmail.com>
2023-10-14 19:03:15 -0400

    Adding more actions

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,40 @@ const program = new Command();
 // Add actions onto that CLI
 
 program
-  .action(()=>{
-    console.log("Hello!");
+  .argument("<string>", "string to long")
+  .option("-c, --capitalize", "Capitalize the message")
+  .action(
+    (
+      message: string, 
+      opts: {
+        capitalize?: boolean;
+      }
+    ) => {
+      if (opts.capitalize) {
+        console.log(message.toUpperCase());
+      }
+      else {
+        console.log(message);
+      }
+    }
+  )
+  .description("Print a message");
+
+program
+  .command("add <number...>")
+  .action((numbers: string[]) => {
+    const total = numbers.reduce((a, v) => Number(a) + Number(v), 0);
+    console.log(`Total: ${total}`);
   })
-  .description("Say Hello");
+  .description("Add numbers and log the total");
+
+program
+  .command("get-max-number <number...>")
+  .action((numbers: number[])=>{
+    const max = Math.max(...numbers);
+    console.log(`Max: ${max}`);
+  })
+  .description("Get the higher number due a list of given number");
 
 // Excute the CLI with the given arguments
 
