bf95a6f467032a170ac95ab019952b80ea3ee4d7
Brayan Bernal <brayanbernal0710@gmail.com>
2023-10-14 18:17:37 -0400

    Adding commander and fixing link-cli pkg script

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 #!/usr/bin/env node
 
-const hello = (message: string) => {};
+import { Command } from "commander";
 
-console.log("HELLO AGAIN!!");
+// Declare the program
+
+const program = new Command();
+
+// Add actions onto that CLI
+
+program
+  .action(()=>{
+    console.log("Hello!");
+  })
+  .description("Say Hello");
+
+// Excute the CLI with the given arguments
+
+program.parse(process.argv);
